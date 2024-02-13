d151bca057d3b0a412de5131e0647bcb08d850f6
Joss Gitlin <gitlinjoss@tutanota.com>
2024-02-13 18:27:51 -0500

    Add in unity project

---
+++ Assets/S_RenderTex/Webcam/Editor/WebcamControllerEditor.cs
@@ -0,0 +1,63 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Khoreo {
+
+[CustomEditor(typeof(WebcamController))]
+sealed class WebcamControllerEditor : Editor
+{
+    SerializedProperty _deviceName;
+    SerializedProperty _threshold;
+    SerializedProperty _contrast;
+    SerializedProperty _renderTexture;
+
+    static GUIContent _selectLabel = new GUIContent("Select");
+
+    void OnEnable()
+    {
+        _deviceName = serializedObject.FindProperty("_deviceName");
+        _threshold = serializedObject.FindProperty("_threshold");
+        _contrast = serializedObject.FindProperty("_contrast");
+        _renderTexture = serializedObject.FindProperty("_texture");
+    }
+
+    void ChangeWebcam(string name)
+    {
+        serializedObject.Update();
+        _deviceName.stringValue = name;
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void ShowDeviceSelector(Rect rect)
+    {
+        var menu = new GenericMenu();
+        foreach (var device in WebCamTexture.devices)
+            menu.AddItem(new GUIContent(device.name), false,
+                         () => ChangeWebcam(device.name));
+        menu.DropDown(rect);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUI.BeginDisabledGroup(Application.isPlaying);
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PropertyField(_deviceName);
+        var rect = EditorGUILayout.GetControlRect(false, GUILayout.Width(60));
+        if (EditorGUI.DropdownButton(rect, _selectLabel, FocusType.Keyboard))
+            ShowDeviceSelector(rect);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.PropertyField(_threshold);
+        EditorGUILayout.PropertyField(_contrast);
+        EditorGUILayout.PropertyField(_renderTexture);
+
+        EditorGUI.EndDisabledGroup();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
+} // namespace Khoreo
